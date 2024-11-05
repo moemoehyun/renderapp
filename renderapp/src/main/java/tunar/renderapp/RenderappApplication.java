13d809079e8c27bcc0e49e320b2890353c84b01c
なとり <167410463+moemoehyun@users.noreply.github.com>
2024-11-05 14:25:11 +0900

    first commit

---
+++ renderapp/src/main/java/tunar/renderapp/RenderappApplication.java
@@ -0,0 +1,11 @@
+package tunar.renderapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RenderappApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RenderappApplication.class, args);
+    }
+}
---
+++ renderapp/src/test/java/tunar/renderapp/RenderappApplicationTests.java
@@ -0,0 +1,13 @@
+package tunar.renderapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RenderappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
